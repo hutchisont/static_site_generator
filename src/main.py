c4c8fa8dfe23c68f61f4aa14bca58d2e8a470827
hutchisont <24536068+hutchisont@users.noreply.github.com>
2024-04-22 21:17:50 -0500

    feat: create initial TextNode class

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+
+
+def main():
+    testNode = TextNode("Test node", "bold", "http://localhost:8888")
+    print(testNode)
+
+
+if __name__ == "__main__":
+    main()
