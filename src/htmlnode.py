6fad78c5a92d9b63bafdb9edb691d423f5b64ec2
hutchisont <24536068+hutchisont@users.noreply.github.com>
2024-04-23 00:39:16 -0500

    refactor: move leafnode to same file as htmlnode
    not sure about this one really, maybe this gets changed back
    later but easier to just shove them together for now I guess

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -27,3 +27,20 @@ value: {self.value}
 children: {self.children}
 props: {self.props}
         """
+
+
+class LeafNode(HTMLNode):
+
+    def __init__(self,
+                 tag: str,
+                 value: str,
+                 props: dict[str, str] = None
+                 ):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("It is required for leaf nodes to have a value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class HTMLNodeTest(unittest.TestCase):
@@ -14,4 +14,12 @@ class HTMLNodeTest(unittest.TestCase):
                         })
         actual = node.props_to_html()
         expected = " testkey=\"test val\" testkey2=\"test val2\""
+        self.assertEqual(actual, expected)
+
+
+class LeafNodeTest(unittest.TestCase):
+    def test_leafnode_to_html_returns_expected_html(self):
+        node = LeafNode("p", "test paragraph of text.")
+        actual = node.to_html()
+        expected = "<p>test paragraph of text.</p>"
         self.assertEqual(actual, expected)
