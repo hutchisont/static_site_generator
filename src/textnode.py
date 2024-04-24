a8066afbea8b64d1e79658e0ab67b1b3bbdf0559
hutchisont <24536068+hutchisont@users.noreply.github.com>
2024-04-23 22:22:14 -0500

    feat: convert text ndoes to leaf ndoes

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,14 @@
+from htmlnode import LeafNode
+
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
 class TextNode:
     def __init__(self, text: str, text_type: str, url: str = None):
         self.text = text
@@ -13,3 +24,22 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == text_type_text:
+        return LeafNode("", text_node.text)
+    if text_node.text_type == text_type_bold:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == text_type_italic:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == text_type_code:
+        return LeafNode("c", text_node.text)
+    if text_node.text_type == text_type_link:
+        return LeafNode("a", text_node.text)
+    if text_node.text_type == text_type_image:
+        return LeafNode("img",
+                        "",
+                        {"src": text_node.url, "alt": text_node.text})
+
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
