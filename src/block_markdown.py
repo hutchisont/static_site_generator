bd3444bdf6b41ade30d092eecc8578799fa664fd
hutchisont <24536068+hutchisont@users.noreply.github.com>
2024-05-05 16:10:03 -0500

    feat: block_to_block_type

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,11 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
+
 def markdown_to_blocks(document_text: str) -> list:
     split_text = document_text.split("\n\n")
     blocks = []
@@ -6,3 +14,61 @@ def markdown_to_blocks(document_text: str) -> list:
             continue
         blocks.append(line.strip())
     return blocks
+
+
+def block_to_block_type(input_block: str) -> str:
+    split_block = input_block.split('\n')
+    if _check_for_heading(split_block) is True:
+        return block_type_heading
+    if _check_for_code(split_block) is True:
+        return block_type_code
+    if _check_for_quote(split_block) is True:
+        return block_type_quote
+    if _check_for_unordered_list(split_block) is True:
+        return block_type_unordered_list
+    if _check_for_ordered_list(split_block) is True:
+        return block_type_ordered_list
+    return block_type_paragraph
+
+
+def _check_for_heading(lines_to_check: list) -> bool:
+    headings = ("#", "##", "###", "####", "#####", "######")
+    for line in lines_to_check:
+        if not line.startswith(headings):
+            return False
+
+    return True
+
+
+def _check_for_code(lines_to_check: list) -> bool:
+    code_block_tag = "```"
+    if len(lines_to_check) > 2:
+        return (lines_to_check[0] == code_block_tag and
+                lines_to_check[-1] == code_block_tag)
+
+    return False
+
+
+def _check_for_quote(lines_to_check: list) -> bool:
+    for line in lines_to_check:
+        if not line.startswith(">"):
+            return False
+    return True
+
+
+def _check_for_unordered_list(lines_to_check: list) -> bool:
+    tags = ("* ", "- ")
+    for line in lines_to_check:
+        if not line.startswith(tags):
+            return False
+
+    return True
+
+
+def _check_for_ordered_list(lines_to_check: list) -> bool:
+    for i in range(0, len(lines_to_check)):
+        tag = f"{i+1}. "
+        if not lines_to_check[i].startswith(tag):
+            return False
+
+    return True
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -2,6 +2,13 @@ import unittest
 
 from block_markdown import (
     markdown_to_blocks,
+    block_to_block_type,
+    block_type_heading,
+    block_type_code,
+    block_type_quote,
+    block_type_paragraph,
+    block_type_unordered_list,
+    block_type_ordered_list,
 )
 
 
@@ -27,6 +34,102 @@ This is the same paragraph on a new line""",
             ],
             blocks,
         )
+
+    def test_block_to_block_type_heading(self):
+        test_block_good = """# one
+## two
+### three
+#### four
+##### five
+###### six"""
+
+        block_type = block_to_block_type(test_block_good)
+        self.assertEqual(block_type, block_type_heading)
+
+        test_block_bad = """not a heading"""
+        block_type = block_to_block_type(test_block_bad)
+        self.assertNotEqual(block_type, block_type_heading)
+
+        test_block_bad = """# this is but
+this is not a heading
+######## nor is this"""
+        block_type = block_to_block_type(test_block_bad)
+        self.assertNotEqual(block_type, block_type_heading)
+
+    def test_block_to_block_type_code(self):
+        test_block_good = """```
+for fail in life:
+    cringe
+```"""
+        block_type = block_to_block_type(test_block_good)
+        self.assertEqual(block_type, block_type_code)
+
+        test_block_bad = """not code block"""
+        block_type = block_to_block_type(test_block_bad)
+        self.assertNotEqual(block_type, block_type_code)
+
+        test_block_bad = """```
+not code block"""
+        block_type = block_to_block_type(test_block_bad)
+        self.assertNotEqual(block_type, block_type_code)
+
+    def test_block_to_block_type_quote(self):
+        test_block_good = """> all in life is
+> rather difficult at times"""
+
+        block_type = block_to_block_type(test_block_good)
+        self.assertEqual(block_type, block_type_quote)
+
+        test_block_bad = """not quote block"""
+        block_type = block_to_block_type(test_block_bad)
+        self.assertNotEqual(block_type, block_type_quote)
+
+        test_block_bad = """> not quote block
+but partially is"""
+        block_type = block_to_block_type(test_block_bad)
+        self.assertNotEqual(block_type, block_type_quote)
+
+    def test_block_to_block_type_unordered_list(self):
+        test_block_good = """* item
+* another
+- yet another"""
+
+        block_type = block_to_block_type(test_block_good)
+        self.assertEqual(block_type, block_type_unordered_list)
+
+        test_block_bad = """not unordered list block"""
+        block_type = block_to_block_type(test_block_bad)
+        self.assertNotEqual(block_type, block_type_unordered_list)
+
+        test_block_bad = """* partial list block
+but partially is"""
+        block_type = block_to_block_type(test_block_bad)
+        self.assertNotEqual(block_type, block_type_unordered_list)
+
+    def test_block_to_block_type_ordered_list(self):
+        test_block_good = """1. item
+2. second
+3. third"""
+
+        block_type = block_to_block_type(test_block_good)
+        self.assertEqual(block_type, block_type_ordered_list)
+
+        test_block_bad = """not ordered list block"""
+        block_type = block_to_block_type(test_block_bad)
+        self.assertNotEqual(block_type, block_type_ordered_list)
+
+        test_block_bad = """1. partial ordered list block
+3. but not numbered correctly"""
+        block_type = block_to_block_type(test_block_bad)
+        self.assertNotEqual(block_type, block_type_ordered_list)
+
+    def test_block_to_block_type_paragraph(self):
+        test_block_good = """just some random text, not like
+there is good or bad to a paragraph block
+its just some random text"""
+
+        block_type = block_to_block_type(test_block_good)
+        self.assertEqual(block_type, block_type_paragraph)
 
 
 if __name__ == "__main__":
