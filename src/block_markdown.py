dc1b18b50d15372055508fee3bfe84574e9cd8c1
hutchisont <24536068+hutchisont@users.noreply.github.com>
2024-05-06 01:31:43 -0500

    feat: .md document to html nodes

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,8 @@
+from htmlnode import ParentNode
+from inline_markdown import text_to_textnodes
+from textnode import text_node_to_html_node
+
+
 block_type_paragraph = "paragraph"
 block_type_heading = "heading"
 block_type_code = "code"
@@ -72,3 +77,105 @@ def _check_for_ordered_list(lines_to_check: list) -> bool:
             return False
 
     return True
+
+
+def markdown_to_html_node(markdown_document: str) -> list:
+    blocks = markdown_to_blocks(markdown_document)
+    html_nodes = []
+    for block in blocks:
+        node = block_to_html_node(block)
+        html_nodes.append(node)
+
+    return ParentNode("div", html_nodes, None)
+
+
+def block_to_html_node(block: str):
+    block_type = block_to_block_type(block)
+    if block_type == block_type_heading:
+        return _heading_block_to_html_node(block)
+    if block_type == block_type_code:
+        return _code_block_to_html_node(block)
+    if block_type == block_type_quote:
+        return _quote_block_to_html_node(block)
+    if block_type == block_type_ordered_list:
+        return _ordered_list_block_to_html_node(block)
+    if block_type == block_type_unordered_list:
+        return _unordered_list_block_to_html_node(block)
+    if block_type == block_type_paragraph:
+        return _paragraph_block_to_html_node(block)
+    raise ValueError("Invalid block type")
+
+
+def text_to_children(text: str):
+    nodes = text_to_textnodes(text)
+    children = []
+
+    for node in nodes:
+        html_node = text_node_to_html_node(node)
+        children.append(html_node)
+
+    return children
+
+
+def _heading_block_to_html_node(block: str):
+    level = 0
+    for char in block:
+        if char == "#":
+            level += 1
+        else:
+            break
+
+    if level + 1 >= len(block):
+        raise ValueError(f"Invalid heading level: {level}")
+
+    text = block[level + 1:]
+    children = text_to_children(text)
+    return ParentNode(f"h{level}", children)
+
+
+def _code_block_to_html_node(block: str):
+    if not _check_for_code(block):
+        raise ValueError("Invalid code block")
+    text = block[4:-3]
+    children = text_to_children(text)
+    code = ParentNode("code", children)
+    return ParentNode("pre", [code])
+
+
+def _quote_block_to_html_node(block: str):
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError("Invalid quote block")
+        new_lines.append(line.lstrip(">").strip())
+    content = " ".join(new_lines)
+    children = text_to_children(content)
+    return ParentNode("blockquote", children)
+
+
+def _ordered_list_block_to_html_node(block: str):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[3:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ol", html_items)
+
+
+def _unordered_list_block_to_html_node(block: str):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[2:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ul", html_items)
+
+
+def _paragraph_block_to_html_node(block: str):
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode("p", children)
--- src/textnode.py
+++ src/textnode.py
@@ -28,15 +28,15 @@ class TextNode:
 
 def text_node_to_html_node(text_node):
     if text_node.text_type == text_type_text:
-        return LeafNode("", text_node.text)
+        return LeafNode(None, text_node.text)
     if text_node.text_type == text_type_bold:
         return LeafNode("b", text_node.text)
     if text_node.text_type == text_type_italic:
         return LeafNode("i", text_node.text)
     if text_node.text_type == text_type_code:
-        return LeafNode("c", text_node.text)
+        return LeafNode("code", text_node.text)
     if text_node.text_type == text_type_link:
-        return LeafNode("a", text_node.text)
+        return LeafNode("a", text_node.text, {"href": text_node.url})
     if text_node.text_type == text_type_image:
         return LeafNode("img",
                         "",
